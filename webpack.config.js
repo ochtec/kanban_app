57b6c7dbe6d7b68d68ad8e1dcc9fd43643752114
miketoc4 <miketoc6@gmail.com>
2016-04-11 17:19:31 -0400

    hot loading complete

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
+
+process.env.BABEL_ENV = TARGET;
+
 const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
